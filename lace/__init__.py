a6db3d8fdb482f8d1c0956b31f0ddef4b6b7ff2a
Nour-Alhussien <108490194+Nour-Alhussien@users.noreply.github.com>
2022-08-10 05:33:06 -0400

    Update __init__.py

--- lace/__init__.py
+++ lace/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+from __future__ import absolute_import
 from ._version import get_versions
-from CLIFF import cliff
-from MORPH import morph
-from LACE1 import lace1
-from LACE2 import add_to_bin, lace2_simulator
+from .CLIFF import cliff
+from .MORPH import morph
+from .LACE1 import lace1
+from .LACE2 import add_to_bin, lace2_simulator
 
 __author__ = 'Jianfeng Chen'
 __email__ = 'jchen37@ncsu.edu'
 __version__ = get_versions()['version']
 del get_versions
-
